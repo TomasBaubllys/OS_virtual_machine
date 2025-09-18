753c5bf6da76ace9efaf4f25aaac19c89a8fa098
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-09-18 23:27:26 +0300

    changed memory to a single array for easier use

--- include/memory.h
+++ include/memory.h
@@ -1,23 +1,33 @@
 #ifndef MEMORY_H_INCLUDED
 #define MEMORY_H_INCLUDED
 
-#define _MEM_WORD_SIZE 4
+#define MEM_WORD_SIZE 4
 
-#define _MEM_USER_MEMORY_SIZE 816
-#define _MEM_SUPERVISOR_MEMORY_SIZE 64
-#define _MEM_SHARED_MEMORY_SIZE 32
+#define MEM_USER_MEMORY_SIZE 816
+#define MEM_SUPERVISOR_MEMORY_SIZE 64
+#define MEM_SHARED_MEMORY_SIZE 32
+
+#define MEM_TOTAL_MEMORY MEM_USER_MEMORY_SIZE + MEM_SHARED_MEM_SIZE + MEM_SUPERVISOR_MEMORY_SIZE
+
+#define MEM_BAD_ADDRESS_ERR "Maximum address exceeded" 
 
 #include <string.h>
+#include <stdio.h>
 
 typedef struct Memory {
-	uint32_t user[_MEM_USER_MEMORY_SIZE];
-	uint32_t shared[_MEM_SHARED_MEM_SIZE];
-	uint32_t supervisor[_MEM_SUPERVISOR_MEMORY_SIZE];
-
+	// uint32_t user[MEM_USER_MEMORY_SIZE];
+	// uint32_t shared[MEM_SHARED_MEM_SIZE];
+	// uint32_t supervisor[MEM_SUPERVISOR_MEMORY_SIZE];
+	uint32_t memory[MEM_TOTAL_MEMORY]
 } Memory;
 
 int init_memory(Memory* mem);
-uint32_t read_word(Memory* mem); 					// not implemented maybe ad a mem_mode? as in user - supervisor
-int write_word(Memory* mem, uint16_t address, uint32_t word); 		// not implemented yet
+
+uint32_t read_word(Memory* mem, const uint16_t address); 			
+
+int write_word(Memory* mem, const uint16_t address, const uint32_t word); 
+ 
+// prints memory from start address till end address, they must be aligned to a word a.k.a divisible by 4
+void print_memory(Memory* mem, uint16_t start, uint16_t end);
 
 #endif
--- src/memory.c
+++ src/memory.c
@@ -5,9 +5,31 @@ int init_memory(Memory* mem) {
 		return -1;
 	}	
 
-	memset(mem -> user, 0, sizeof(mem -> user));
-	memset(mem -> shared, 0, sizeof(mem -> share));
-	memset(mem -> supervisor, 0, sizeof(mem -> supervisor));
+	// memset(mem -> user, 0, sizeof(mem -> user));
+	// memset(mem -> shared, 0, sizeof(mem -> share));
+	// memset(mem -> supervisor, 0, sizeof(mem -> supervisor));
+	memset(mem -> memory, 0, sizeof(mem -> memory));	
+
+	return 0;
+}
+
+
+uint32_t read_word(Memory* mem, const uint16_t address) {
+	if(address > MEM_TOTAL_MEMORY)	{
+		fprintf(stderr, MEM_BAD_ADDRESS_ERR);
+		return 0;
+	}
 	
+	return mem -> memory[address];
+} 			
+
+int write_word(Memory* mem, const uint16_t address, const uint32_t word) {
+	if(address > MEM_TOTAL_MEMORY) {
+		fprintf(stderr, MEM_BAD_ADDRESS_ERR);
+		return -1;
+	}
+	
+	mem -> memory[address] = word;
+
 	return 0;
 }
