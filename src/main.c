dd40ba49c5de47149c4f63b1d9ecf944dd717217
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2025-09-25 00:29:07 +0300

    added mini program

--- src/main.c
+++ src/main.c
@@ -12,25 +12,28 @@ int main(int argc, char* argv[]) {
 	assert(add_virtual_machine(&real_machine, &vm) == 0);
 
 	// test program
-	uint32_t test_program[] = {0x4D4F3032, 0x4C525242, 0x4D4F3066, 0x4D555242, 0x50555441}; // after this ra register should 30 (2 * f)
+	uint32_t test_program[] = {0x4D4F3032, 0x4C525242, 0x4D4F3066, 0x4D555242, 0x47454441, 0x50555441}; // after this ra register should 30 (2 * f)
 
-	load_program_virtual_machine(&real_machine, 0, 0, test_program, 5);
+	load_program_virtual_machine(&real_machine, 0, 0, test_program, 6);
 	
 	// fprint_memory(stdout, &(real_machine.mem), 0, MEM_MAX_ADDRESS - 1, 16);
 	
-	while(1) {
+	for(int i = 0; i < 10; ++i) {
 		copy_virtual_machine(&(real_machine), 0);
-		execute_command(&(real_machine), 0, real_machine.mem.memory[real_machine.vm[0].pc]);
-		write_virtual_machine(&real_machine, 0);	
+		uint32_t command = read_word(&(real_machine.mem), real_machine.cpu.pc);
+		execute_command(&(real_machine), 0, command);
 				
 		if(real_machine.cpu.pi + real_machine.cpu.si > 0) {
-			printf("here\n");
 			if(xchg(&real_machine) != 0) {
 				// do something
+				// kill the vm
+				// if stop encoutered also kill the vm
 			}
 			
 			pi_si_reset(&real_machine);
 		}
+		
+		write_virtual_machine(&real_machine, 0);	
     	}	
         
 	return 0;
