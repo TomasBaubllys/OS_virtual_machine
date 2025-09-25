15c773117ea560845f4a4d45d3d7897978a233fc
godagu <goda.gutparakyte@mif.stud.vu.lt>
2025-09-25 21:48:35 +0300

    menu conflict solved

--- include/menu.h
+++ include/menu.h
@@ -1,6 +1,7 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
 
+<<<<<<< HEAD
 #include "real_machine.h"
 #define MENU_ON 0
 #define ADD_VM 1  
@@ -16,11 +17,40 @@
 
 #define MENU_MEM_DUMP_COL_COUNT 16
 
+#define MSG_MENU "MENU:\n"
+#define MSG_LOAD_VM "Load a virtual machine\n"
+#define MSG__UNLOAD_VM "Unload a virtual machine\n"
+#define MSG_SHOW_MEMORY "Show memory\n"
+#define MSG_LOAD_PROGRAM "Load a program\n"
+#define MSG_CHOOSE_VM "Please choose a virtual machine:\n"
+#define MSG_START_VM "Start a virtual machine\n"
+#define MSG_SELECT_LOADED_VM "Please select a virtual machine\n"
+#define MSG_RUN_PROGRAM "Run program\n"
+#define MSG_SELECT_LOADED_RPGRAMS "Please select a loaded program to run\n"
+#define MSG_STOP_VMI "Stop virtual machine\n"
+#define MSG_RUN_PRGRM_NORMAL "Run program in normal mode\n"
+#define MSG_RUN_PRGRM_TRACE "Run program in trace mode\n"
+
 void dump_memory(Real_machine* real_machine);
 
 int display_menu();
 
 uint8_t menu(Real_machine* real_machine);
 
-#endif // MENU_H_INCLUDED
+	// MENU
+	// load VM (max 3)
+		// show loaded VMs
+	// unload VM
+		// show loaded VMs
+	// show memory (dump)
+	// load a program
+		// - chose vm
+	// start VM (max 3)
+		// show started VMs
+		// - Run program
+			// - Select from loaded programs
+				// - Run program normal mode
+				// - Run program trace mode
+		// - Unstart VM 
 
+#endif // MENU_H_INCLUDED
